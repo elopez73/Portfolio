61c2ebb497f7f36bb1326a0be1559894bf5f1853
Esau Lopez <esaulopez14@gmail.com>
2022-12-11 13:15:42 -0700

    NEw3

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import classes from "./HomeLayout.module.css";
 const BG = new URL("./Background.jpg", import.meta.url);
 function Home(){
     return<section id="Home" className={classes.Infos}>
-        <img src={BG} className={classes.bg} atl="None"></img>
+        <img src={BG} className={classes.bg} alt="None"></img>
         <Opening intro ="Hi my Name is," Name="Esau Lopez" p1="Aspiring Web Developer" />
 
         <div className={classes.grid}>
