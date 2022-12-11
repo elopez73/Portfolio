e07381e85bd3f4fae8498d391ac3ab16f6c81244
Esau Lopez <esaulopez14@gmail.com>
2022-12-11 14:37:53 -0700

    N*

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,15 @@
-import Info from "./compenents/Info";
+
 import Opening from "./compenents/Opening";
 import classes from "./HomeLayout.module.css";
 const BG = new URL("./Background.jpg", import.meta.url);
 function Home(){
     return<section id="Home" className={classes.Infos}>
         <img src={BG} className={classes.bg} alt="None"></img>
-        <Opening intro ="Hi my Name is," Name="Esau Lopez" p1="Aspiring Web Developer" />
+        <Opening intro ="Hi my Name is," Name="Esau Lopez" p1="Web Developer" />
 
         <div className={classes.grid}>
+        <Opening   p1="To learn about me, see my pojects and contact me click above." />
 
-            <Info id="1" text="First" />
-            <Info id="4" text="Fourth" />
 
        </div>
 
