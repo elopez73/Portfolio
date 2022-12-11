7f79ad57856c9f17f7abfb57a0a2cad3caf752ea
Esau Lopez <esaulopez14@gmail.com>
2022-12-11 13:47:24 -0700

    Updates

--- src/About.js
+++
@@ -1,9 +0,0 @@
-import Opening from './compenents/Opening';
-import Info from './compenents/Info';
-function About(){
-return<section>
-    <Opening Name="About me" intro="I love Programing"/>
-    <Info id="2" text="Second" />
-</section>
-}
-export default About;
